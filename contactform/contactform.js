5258373f8cb180b73ff12ab1fd218d5805814063
haizhuhong <hong8890@163.com>
2021-05-27 03:46:23 +0800

    Update contactform.js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
   "use strict";
 
   //Contact
-  $('form.contactForm').submit(function() {
+  $('form.contactForm').submit(function(e) {
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
@@ -90,6 +90,8 @@ jQuery(document).ready(function($) {
     });
     if (ferror) return false;
     else var str = $(this).serialize();
+
+    e.preventDefault();
 
     $.ajax({
       type: "POST",
