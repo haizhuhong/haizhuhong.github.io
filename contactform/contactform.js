bdfaf4219577fbeaf02bde7a8f5d5c64c4585f93
haizhuhong <hong8890@163.com>
2021-05-27 23:52:47 +0800

    update contact form and bootstrap

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -12,11 +12,9 @@ var form = document.getElementById("my-form");
         }
       }).then(response => {
         status.innerHTML = "Thanks for your submission!";
-        alert("Thanks for your submission!");
         form.reset();
       }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form.";
-        alert("Oops! There was a problem submitting your form.");
+        status.innerHTML = "Oops! There was a problem submitting your form."
       });
     }
     form.addEventListener("submit", handleSubmit)
